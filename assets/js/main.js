915518f7f8bd76d6ee4300874b3ae8a91ab56aca
psuskin <philsuskin@gmail.com>
2023-11-10 05:43:14 +0100

    Implemented carousels

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,3 +39,32 @@ $(document).on("click", ".img-c.active", function () {
     copy.remove();
   }, 500)
 })
+
+"use strict";
+
+var carousels = function () {
+  $(".owl-carousel1").owlCarousel({
+    loop: true,
+    center: true,
+    margin: 0,
+    responsiveClass: true,
+    nav: false,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false
+      },
+      680: {
+        items: 2,
+        nav: false,
+        loop: false
+      },
+      1000: {
+        items: 3,
+        nav: true
+      }
+    }
+  });
+};
+
+carousels();
