a23d64ce87c2078453346b1bc9a3dc248c8a3f5a
psuskin <philsuskin@gmail.com>
2023-11-10 03:13:07 +0100

    Decent start, but transition starts from top of screen

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,42 @@
-window.onscroll = function() {
-    var header = document.getElementById("top");
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        header.classList.add("gray");
-    } else {
-        header.classList.remove("gray");
-    }
+window.onscroll = function () {
+  var header = document.getElementById("top");
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    header.classList.add("gray");
+  } else {
+    header.classList.remove("gray");
+  }
 };
+
+$(function () {
+  $(".img-w").each(function () {
+    $(this).wrap("<div class='img-c'></div>")
+    let imgSrc = $(this).find("img").attr("src");
+    $(this).css('background-image', 'url(' + imgSrc + ')');
+  })
+
+
+  $(".img-c").click(function () {
+    let w = $(this).outerWidth()
+    let h = $(this).outerHeight()
+    let x = $(this).offset().left
+    let y = $(this).offset().top
+
+    $(".active").not($(this)).remove()
+    let copy = $(this).clone();
+    copy.insertAfter($(this)).height(h).width(w).delay(500).addClass("active")
+    $(".active").css('top', y - 8);
+    $(".active").css('left', x - 8);
+
+    setTimeout(function () {
+      copy.addClass("positioned")
+    }, 0)
+  })
+})
+
+$(document).on("click", ".img-c.active", function () {
+  let copy = $(this);
+  copy.removeClass("positioned active").addClass("postactive");
+  setTimeout(function () {
+    copy.remove();
+  }, 500)
+})
